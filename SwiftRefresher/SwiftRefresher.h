55c235cd4f52087c291f26d80ae0f040abbbfcbb
Princess Sampson <ps@kingonomics.com>
2016-09-20 19:06:37 -0400

    Initial Commit

---
+++ SwiftRefresher/SwiftRefresher.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRefresher.h
+//  SwiftRefresher
+//
+//  Created by Princess Sampson on 9/20/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftRefresher.
+FOUNDATION_EXPORT double SwiftRefresherVersionNumber;
+
+//! Project version string for SwiftRefresher.
+FOUNDATION_EXPORT const unsigned char SwiftRefresherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRefresher/PublicHeader.h>
+
+
